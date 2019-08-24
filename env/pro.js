5f6328a15d7c69de6522708c5eac942d355ecc52
caohenghu <caohenghu@gmail.com>
2019-08-24 16:45:14 +0800

    修改打包配置

--- env/pro.js
+++ env/pro.js
@@ -2,7 +2,7 @@ module.exports = {
     host: {
         cdn: 'caohenghu.github.io'
     },
-    plugin: {
-        vue: '//lib.eqh5.com/vue/2.5.13/vue.runtime.min.js'
-    }
+    plugin: [
+        '//cdn.bootcss.com/vue/2.5.13/vue.runtime.min.js'
+    ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,62 @@
 const webpack = require('webpack')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path')
+
+const minify = {
+    minifyCSS: true,
+    minifyJS: true,
+    removeComments: true, // 删除HTML中的注释
+    collapseWhitespace: true, // 删除空白符与换行符
+    collapseBooleanAttributes: true, // 省略布尔属性的值 <input checked="true"/> ==> <input checked />
+    removeEmptyAttributes: true, // 删除所有空格作属性值 <input id="" /> ==> <input />
+    removeScriptTypeAttributes: true, // 删除script上的type
+    removeStyleLinkTypeAttributes: true // 删除style上的type
+}
+
+const rules = [
+    {
+        test: /\.vue$/,
+        use: 'vue-loader'
+    },
+    // {
+    //     test: /\.js$/,
+    //     exclude: /node_modules/,
+    //     use: 'babel-loader'
+    // },
+    {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+    },
+    {
+        test: /\.(png|jpg|gif|svg|ico)$/,
+        use: [
+            {
+                loader: 'url-loader',
+                options: {
+                    name: 'images/[name]-[hash:8].[ext]',
+                    limit: 1000
+                }
+            }
+        ]
+    }
+]
 
 module.exports = options => {
     const env = require('./env/' + options.config + '.js')
     const plugin = require('./env/pro.js').plugin
     const port = env.port || 5555
-    const buildDir = 'dist'
     const isLocal = options.local
-    const editorLibs = []
-    const minify = {
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: true, // 删除HTML中的注释
-        collapseWhitespace: true, // 删除空白符与换行符
-        collapseBooleanAttributes: true, // 省略布尔属性的值 <input checked="true"/> ==> <input checked />
-        removeEmptyAttributes: true, // 删除所有空格作属性值 <input id="" /> ==> <input />
-        removeScriptTypeAttributes: true, // 删除script上的type
-        removeStyleLinkTypeAttributes: true // 删除style上的type
-    }
-
-    // 如果是本地开发，使用未压缩的插件
-    if (isLocal) {
-        for (let key in plugin) {
-            plugin[key] = plugin[key].replace('.min', '')
-        }
-    }
-    for (let key in plugin) {
-        editorLibs.push({
-            url: plugin[key],
-            isAsync: false
-        })
-    }
-
-    const rules = [
-        {
-            test: /\.vue$/,
-            use: 'vue-loader'
-        },
-        // {
-        //     test: /\.js$/,
-        //     exclude: /node_modules/,
-        //     use: 'babel-loader'
-        // },
-        {
-            test: /\.scss$/,
-            use: ['vue-style-loader', 'css-loader', 'sass-loader']
-        },
-        {
-            test: /\.(png|jpg|gif|svg|ico)$/,
-            use: [
-                {
-                    loader: 'url-loader',
-                    options: {
-                        name: 'images/[name]-[hash:8].[ext]',
-                        limit: 1000
-                    }
-                }
-            ]
-        }
-    ]
-
+    const libs = []
     const plugins = [
-        new CleanWebpackPlugin(buildDir, {
-            root: __dirname // 需要指定根目录，才能删除项目外的文件夹
-        }),
+        new CleanWebpackPlugin(),
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
             minify,
-            libs: editorLibs
+            libs
         })
     ]
 
@@ -80,37 +64,41 @@ module.exports = options => {
         plugins.push(new webpack.HotModuleReplacementPlugin())
     }
 
+    // 如果是本地开发，使用未压缩的插件
+    if (isLocal) {
+        plugin.forEach((item, i) => {
+            plugin[i] = item
+                .replace('.min', '')
+        })
+    }
+    plugin.forEach(item => {
+        libs.push({
+            url: item,
+            isAsync: false
+        })
+    })
+
     return {
         mode: isLocal ? 'development' : 'production',
         entry: {
-            app: ['./src']
+            app: './src'
         },
         output: {
-            publicPath: options.hot ? '' : '//' + env.host.cdn + '/vue-colorpicker/',
-            path: path.resolve(__dirname, buildDir),
+            publicPath: isLocal ? '' : '//' + env.host.cdn + '/vue-colorpicker/',
+            path: path.resolve('dist'),
             filename: isLocal ? 'js/[name].js' : 'js/[name]-[hash:8].js',
-            chunkFilename: isLocal ? 'js/[name].js' : 'js/[name]-[hash:8].js' // 本地开发如果使用hash，watch会影响到公用js
         },
         module: {
             rules
         },
         plugins,
-        resolve: {
-            alias: {
-                env: path.resolve(__dirname, 'env/' + options.config + '.js'),
-                src: path.resolve(__dirname, 'src')
-            }
-        },
         externals: {
             vue: 'Vue'
         },
         devtool: options.pro ? false : 'source-map',
         devServer: {
             hot: true,
-            port,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
+            port
         }
     }
 }
